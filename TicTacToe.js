09dc1c43e68e1c64e66b71bc6df3be66bc297e26
Chyme <rukessel@gmail.com>
2017-12-01 09:46:32 -0800

    added X's and O's
    color changing makes not text show up in process of fixing this

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,31 @@
+//holds x or o
+var character = "<div class = 'oCSS'>X</div>";
+
+//changes colors fo X's and O's
+
+}
 
 //handles X and O turns
 function turn(location) {
-	document.getElementById(location).innerHTML = "X";
+	document.getElementById(location).innerHTML = character;
+	if(character == "<div class = 'oCSS'>O</div>"){
+		character = "<div class = 'xCSS'>X</div>";
+	}else{
+		character = "<div class = 'oCSS'>O</div>";
+	}
+
+}
+
+//clears X's and O's
+function clearALL(){
+	document.getElementById("r1c1").innerHTML = "";
+	document.getElementById("r1c2").innerHTML = "";
+	document.getElementById("r1c3").innerHTML = "";
+	document.getElementById("r2c1").innerHTML = "";
+	document.getElementById("r2c2").innerHTML = "";
+	document.getElementById("r2c3").innerHTML = "";
+	document.getElementById("r3c1").innerHTML = "";
+	document.getElementById("r3c2").innerHTML = "";
+	document.getElementById("r3c3").innerHTML = "";
+	
 }
